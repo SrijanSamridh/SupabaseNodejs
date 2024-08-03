0d028810edae80c4aa6266afe35486a330eb0fbc
SrijanSamridh <srijansinha3@gmail.com>
2024-08-03 17:29:18 +0530

    test commit

--- app.js
+++ app.js
@@ -2,83 +2,90 @@ const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
-
 
 // using morgan for logs
 app.use(morgan('combined'));
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const supabase = supabaseClient.createClient({
-  apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InptbWN0cWl2Zmh5aWRnY21jb3VtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI2ODM5NjIsImV4cCI6MjAzODI1OTk2Mn0.YTsQPlRevSPivvEAsNJsFSpkto15aYS-IGWL-tWtWio', // public key
-  project: 'zmmctqivfhyidgcmcoum' // project id
-});
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 app.get('/products', async (req, res) => {
-    const {data, error} = await supabase
+    const { data, error } = await supabase
         .from('products')
-        .select()
-    res.send(data);
+        .select();
+    if (error) {
+        res.status(500).send(error);
+    } else {
+        res.send(data);
+    }
 });
 
 app.get('/products/:id', async (req, res) => {
-    const {data, error} = await supabase
+    const { data, error } = await supabase
         .from('products')
         .select()
-        .is('id', req.params.id)
-    res.send(data);
+        .eq('id', req.params.id);
+    if (error) {
+        res.status(500).send(error);
+    } else {
+        res.send(data);
+    }
 });
 
 app.post('/products', async (req, res) => {
-    const {error} = await supabase
+    const { error } = await supabase
         .from('products')
         .insert({
             name: req.body.name,
             description: req.body.description,
             price: req.body.price,
-        })
+        });
     if (error) {
-        res.send(error);
+        res.status(500).send(error);
+    } else {
+        res.send("Created!!");
     }
-    res.send("created!!");
 });
 
 app.put('/products/:id', async (req, res) => {
-    const {error} = await supabase
+    const { error } = await supabase
         .from('products')
         .update({
             name: req.body.name,
             description: req.body.description,
-            price: req.body.price
+            price: req.body.price,
         })
-        .eq('id', req.params.id)
+        .eq('id', req.params.id);
     if (error) {
-        res.send(error);
+        res.status(500).send(error);
+    } else {
+        res.send("Updated!!");
     }
-    res.send("updated!!");
 });
 
 app.delete('/products/:id', async (req, res) => {
-    const {error} = await supabase
+    const { error } = await supabase
         .from('products')
         .delete()
-        .eq('id', req.params.id)
+        .eq('id', req.params.id);
     if (error) {
-        res.send(error);
+        res.status(500).send(error);
+    } else {
+        res.send("Deleted!!");
     }
-    res.send("deleted!!")
-
 });
 
 app.get('/', (req, res) => {
-    res.send("Hello I am working my friend Supabase <3");
+    res.send("Hello, I am working with my friend Supabase <3");
 });
 
 app.get('*', (req, res) => {
-    res.send("Hello again I am working my friend to the moon and behind <3");
+    res.send("Hello again, I am working with my friend to the moon and beyond <3");
 });
 
 app.listen(3000, () => {
